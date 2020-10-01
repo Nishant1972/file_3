f1e8066654faf6758cb4880c1efde84c6ef91663
OmangRawat <56252621+OmangRawat@users.noreply.github.com>
2020-10-02 00:53:24 +0530

    Add files via upload

---
+++ cctryansleak.py
@@ -0,0 +1,27 @@
+try:
+    t = int(input())
+    for i in range(t):
+        n, m, k = [int(x) for x in input().split()]
+        b = []
+        f = []
+        for j in range(n):
+            a = [int(x) for x in input().split()]
+            b.append(a)
+        for j in b:
+            c = []
+            for k in range(1, (m + 1)):
+                d = 0
+                for q in j:
+                    if k == q:
+                        d += 1
+                c.append(d)
+                # print(c)
+            # print(".........")
+            f.append(c.index(max(c)) + 1)
+            # print(f)
+        e = ""
+        for j in f:
+            e += f"{j} "
+        print(e)
+except:
+    pass
